e0b8b2032b17f877ce5b185ccf5c21a9f1760655
Vasanthadithya Mundrathi <vasanthfeb13@gmail.com>
2025-03-21 13:22:30 +0530

    Update geminiService.js

--- src/services/geminiService.js
+++ src/services/geminiService.js
@@ -1,6 +1,6 @@
 import { GoogleGenerativeAI } from '@google/generative-ai';
 
-const API_KEY = 'AIzaSyBnmj4UfDcxRNQbSoiV1xhblEi6bvKoyGE';
+const API_KEY = 'GEMINI KEY';
 const genAI = new GoogleGenerativeAI(API_KEY);
 
 export { genAI };
